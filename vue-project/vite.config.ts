9fffc328cbfa997ad8a9b4390ed8bf0cdd823c69
Milda Rimeikiene <milrim@Valentinass-MBP.lan>
2024-04-20 15:51:28 +0300

    ci: add base

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -12,5 +12,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: 'https://milsi.github.io/github-actions-test'
 })
